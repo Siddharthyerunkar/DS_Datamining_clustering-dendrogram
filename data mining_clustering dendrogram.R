b76c41aba97460d464fc3ba70445b5c928b71147
Siddharthyerunkar <67065966+Siddharthyerunkar@users.noreply.github.com>
2020-06-29 00:46:28 +0530

    Add files via upload

--- data mining_clustering dendrogram.R
+++ data mining_clustering dendrogram.R
@@ -4,8 +4,13 @@ library(factoextra)
 uni.scaled<-scale(Universities[,-1]) #scaling the data
 
 d<-dist(uni.scaled, method = 'euclidean');d #determining distance
+
 d.gram<- hclust(d,method = "centroid") #doing hierarchy clustering
-fviz_dend(d.gram,k=3,k_colors = 1,cex = 0.5,ylab = 'distance')+labs(subtitle = "Dendrgram")
-clust<-cutree(d.gram,k=3) #drawing dendrogram
+
+fviz_dend(d.gram,k=3,k_colors = 1,cex = 0.5,ylab = 'distance')+
+  labs(subtitle = "Dendrgram") #drawing Dendrogram
+
+clust<-cutree(d.gram,k=3) # k means number of cluster after viewing Dendrogram
 clust 
+
 finaluni<-data.frame(Universities="univ",'cluster'=clust) #making data frame 
